bab313bc55dd248cb6c16d6715fb322f45a9882e
hilmi raziq <m.hilmi.raziq@gmail.com>
2023-06-18 16:52:04 +0700

    first commit

--- start.sh
+++ start.sh
@@ -1,17 +1,15 @@
 set -eu
 
-export PYTHONUNBUFFERED-truc
+export PYTHONUNBUFFERED=true
 
 VIRTUALENV=.data/venv
 
 if [ ! -d $VIRTUALENV]; then
     python3 -m venv $VIRTUALENV
-
 fi
 
 if [ ! -f $VIRTUALENV/bin/pip ]; then
     curl --silent --show-error --retry 5 https://bootstrap.pypa.io/get-pip.py | $VIRTUALENV/bin/python
-
 fi
 
 $VIRTUALENV/bin/pip install -r requirements.txt
